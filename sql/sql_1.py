99eb70478892e6f793849ec825a87666b966b849
Abdullah <abdullah.n.2005.3.21@gmail.com>
2026-01-21 13:43:55 +0300

    initial commit

--- sql/sql_1.py
+++ sql/sql_1.py
@@ -1,3 +1,10 @@
 import sqlite3
 
-conn = sqlite3.connect("app.db")
+conn = sqlite3.connect("vocab.db")
+cur = conn.cursor()
+
+cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
+conn.commit()
+
+print("Table users reade")
+conn.close()
