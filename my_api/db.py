ab3603e7a0a2e65f9e29d10acaf94908817e5649
Abdullah <abdullah.n.2005.3.21@gmail.com>
2026-02-26 23:28:25 +0300

    Simple program commit

---
+++ my_api/db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+DB_PATH = "app.db"
+
+def get_conn():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+        id   INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+        )
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS auth_users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password_hash TEXT NOT NULL
+        )
+    """)
+
+    conn.commit()
+    conn.close()
