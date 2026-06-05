6d553b80193a62b6abef661c69cb0c545d04df2b
Abdullah <abdullah.n.2005.3.21@gmail.com>
2026-06-05 09:19:11 +0300

    new TodoProject

--- TodoProject/main.py
+++ TodoProject/main.py
@@ -1,4 +1,4 @@
-import json
+import sqlite3
 
 class Task:
     def __init__(self, task_id, title):
@@ -15,80 +15,101 @@ class Task:
 
 class TaskManager:
     def __init__(self):
-        self.tasks = []
-        self.next_id = 1
-        self.lode_from_file()
-    
+        self.conn = sqlite3.connect("todo.db", check_same_thread=False)
+        self.cur = self.conn.cursor()
+
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TAXT NOT NULL,
+                done INTEGER DEFAULT 0
+            )
+        """)
+
+        self.conn.commit()
+        print("🗄️ تم الاتصال بقاعدة البيانات وتجهيز الجدول بنجاح!")
+
+
+    def get_all_tasks(self):
+
+        self.cur.execute("SELECT id, title, done FROM tasks")
+        rows = self.cur.fetchall()
+
+        tasks_list = []
+        for row in rows:
+
+            task_dict = {
+                "id" : row[0],
+                "title": row[1],
+                "done": bool(row[2])
+            }
+            tasks_list.append(task_dict)
+        
+        return tasks_list
+
+
     def add_task(self, title):
-        new_task = Task(self.next_id, title)
 
-        self.tasks.append(new_task)
+        self.cur.execute("INSERT INTO tasks (title) VALUES (?)", (title,))
 
-        self.next_id += 1
-        print(f"تمت اضافه: {title}")
+        self.conn.commit()
+
+        print(f"تمت الإضافة لقاعدة البيانات: {title}")
     
     def show_tasks(self):
-        print("\n======================")
-        if not self.tasks:
-            print("no itime here")
+        
+        print("\n=== قائمة المهام من قاعدة البيانات ===")
+
+        self.cur.execute("SELECT id, title, done FROM tasks")
+
+        rows = self.cur.fetchall()
+
+        if len(rows) == 0:
+
+            print("قاعدة البيانات فارغة، لا يوجد مهام حالياً.")
+
         else:
-            for task in self.tasks:
+            for row in rows:
+                task_id = row[0]
+                task_title = row[1]
+                task_done = row[2]
+                
+                task = Task(task_id, task_title)
+                task.done = bool(task_done)
                 print(task)
-        print("=======================\n")
+
+        print("==============================================\n")
+
 
     def mark_task_done(self, task_id):
-        for task in self.tasks:
-            if task.id == task_id:
-                task.mark_done()
-                print(f"تم إنجاز المهمة رقم {task_id} بنجاح! 🎉")
-                return
+        self.cur.execute("UPDATE tasks SET done = 1 WHERE id = ?", (task_id,))
+
+        if self.cur.rowcount > 0:
+            self.conn.commit()
+            print(f"✅ تم إنجاز المهمة رقم {task_id} بنجاح!")
         
-        print(f"عذراً، المهمة رقم {task_id} غير موجودة.")
-
-    def save_to_file(self):
-
-        tasks_data = []
-
-        for task in self.tasks:
-            task_dict = {
-                "id": task.id,
-                "title": task.title,
-                "done": task.done
-            }
-            tasks_data.append(task_dict)
-
-        with open("tasks.json", "w", encoding="utf-8") as file:
-            
-            json.dump(tasks_data, file, ensure_ascii=False, indent=4)
-
-        print("💾 تم حفظ المهام في ملف tasks.json بنجاح!")
+        else:
+            print(f"⚠️ عذراً، المهمة رقم {task_id} غير موجودة.")
     
-    def lode_from_file(self):
-        try:
-            with open("tasks.json", "r", encoding="utf-8") as file:
 
-                task_data = json.load(file)
+    def update_task(self, task_id, new_title):
+        self.cur.execute("UPDATE tasks SET title = ? WHERE id = ?", (new_title, task_id))
 
-                self.tasks = []
+        if self.cur.rowcount > 0:
+            self.conn.commit()
+            print(f"✅ تم بنجاح تحديث عنوان المهمه الى {new_title}")
 
-                for data in task_data:
-                    restored_task = Task(data["id"], data["title"])
-
-                    restored_task.done = data["done"]
-
-                    self.tasks.append(restored_task)
-
-                if self.tasks:
-                    self.next_id = self.tasks[-1].id + 1
-
-                print("📂 تم استرجاع المهام السابقة من الملف بنجاح!")
-
-        except FileNotFoundError:
-
-            print("⚠️ لم يتم العثور على ملف سابق، سنبدأ بقائمة فارغة.")
+        else:
+            print(f"⚠️ عذراً، المهمة رقم {task_id} غير موجودة.")
 
 
-manager = TaskManager()
+    def delete_task(self, task_id):
+        self.cur.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
 
-manager.show_tasks()
+        if self.cur.rowcount > 0:
+            self.conn.commit()
+            print(f"🗑️ تم حذف المهمة رقم {task_id} نهائياً.")
+        else:
+            print(f"⚠️ عذراً، لا يمكن الحذف. المهمة رقم {task_id} غير موجودة.")
+
 
