69472453eddc4f3294b96beddc6130d1eac727d7
Abdullah <abdullah.n.2005.3.21@gmail.com>
2026-06-20 08:12:38 +0300

    Add user authentication and validation in login route; implement user model migration

--- isp_project/routes/auth.py
+++ isp_project/routes/auth.py
@@ -1,5 +1,7 @@
 from flask import Blueprint,request, jsonify
 from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -7,16 +9,26 @@ auth_bp = Blueprint('auth', __name__)
 def login():
     data = request.get_json()
 
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({
+            "status": "error",
+            "message": "اسم المستخدم وكلمه المرور مطلوبان"
+        }), 400
+    
     username = data.get('username')
     password = data.get('password')
 
-    if username == 'admin' and password == '123456':
-        access_token = create_access_token(identity=username)
+    user = User.query.filter_by(username=username).first()
+
+    if user and check_password_hash(user.password_hash, password):
+
+        access_token = create_access_token(identity={"username": user.username, "role":user.role})
 
         return jsonify({
             "status": "success",
             "message": "تم تسجيل بنجاح",
-            "token": access_token
+            "token": access_token,
+            "role": user.role
         }), 200
     
     else:
