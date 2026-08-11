3c27bb6370635d6d0de457b46072db9cc5b66846
Abdullah <abdullah.n.2005.3.21@gmail.com>
2026-08-11 15:33:38 +0300

    Fix missing newline at end of file in app.py

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ app.register_blueprint(logging_and_reporting_bp)
 app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0',port=5000, debug=True)
